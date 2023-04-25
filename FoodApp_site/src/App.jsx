59cd0f291ad1b3106a253507ebd5e8a2ef2a0f3f
PrasannaSwain <prasanna.swain@stepapp.ai>
2023-04-26 01:30:47 +0530

    link added

--- FoodApp_site/src/App.jsx
+++ FoodApp_site/src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className='parent' >
       <div className='text'>
-        <span style={{paddingTop:'1rem',position:'absolute'}}>2023 © Praz544</span>
+        <span style={{ paddingTop: '1rem', position: 'absolute' }}>2023 © Praz544</span>
         <h1>FOOD APP</h1>
         <p>Food Recipe Mobile App</p>
-        <p>App Link : <a style={{textDecoration:'underline'}} href=''>Click here</a></p>
+        <p>App Link : <a style={{ textDecoration: 'underline' }} href='https://drive.google.com/drive/folders/16hTPeZnDRgsB_qoj1TnsFnEt8ArOboZ7?usp=share_link' >Click here</a></p>
         <p>Made By <a href='https://github.com/Prasannas544'>Prasanna</a></p>
       </div>
       <div className='image'>
