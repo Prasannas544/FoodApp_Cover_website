7b2e9606bbaad422e04f0d52d6fe930405a2093a
PrasannaSwain <prasanna.swain@stepapp.ai>
2023-04-26 00:21:20 +0530

    withoutApp link

---
+++ FoodApp_site/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='parent' >
+      <div className='text'>
+        <span style={{paddingTop:'1rem',position:'absolute'}}>2023 © Praz544</span>
+        <h1>FOOD APP</h1>
+        <p>Food Recipe Mobile App</p>
+        <p>App Link : <a style={{textDecoration:'underline'}} href=''>Click here</a></p>
+        <p>Made By <a href='https://github.com/Prasannas544'>Prasanna</a></p>
+      </div>
+      <div className='image'>
+      </div>
+    </div>
+  )
+}
+
+export default App
